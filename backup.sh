fd6e81f323386700df705eedd179f44edc237134
Sourabh Pisal <pisal.sourabh@gmail.com>
2024-12-29 18:07:13 +0000

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,11 +1,12 @@
-# Script to backup nextcloud data, database and config files
+#!/bin/bash
 
+# Script to backup Nextcloud data, database, and config files
 # Arguments
 NEXTCLOUD_DIR="$1"
 BACKUP_DIR="$2"
 BACKUP_DIR_DATE_SUFFIX=$(date +%Y%m%d)
 
-CONFIG_FILE=$NEXTCLOUD_DIR/config/config.php
+CONFIG_FILE="$NEXTCLOUD_DIR/config/config.php"
 DATA_DIR=$(grep "'datadirectory'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
 
 DB_NAME=$(grep "'dbname'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
@@ -14,31 +15,38 @@ DB_USER=$(grep "'dbuser'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "',
 DB_PASSWORD=$(grep "'dbpassword'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
 
 # Enable maintenance mode
-sudo -u www-data php --define apc.enable_cli=1 /var/www/nextcloud/occ maintenance:mode --on
+sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --on
 
-# create backup directory
-sudo mkdir -p $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX
-
-# Change dir to backup directory
-cd $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX
+# Create backup directory
+sudo mkdir -p "$BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX"
 
 # Copy data directory
-sudo rsync -Aax $DATA_DIR/ $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX/data/
+sudo rsync -Aax "$DATA_DIR/" "$BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX/data/"
 
 # Copy config directory
-sudo rsync -Aax --exclude='data' $CONFIG_DIR/ $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX/config/
+sudo rsync -Aax --exclude='data' "$NEXTCLOUD_DIR/config/" "$BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX/config/"
 
-# Taking snapshot of database
-sudo mysqldump --single-transaction -default-character-set=utf8mb4 -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > nextcloud-sqlbkp.bak
+# Take database snapshot
+CREDENTIALS_FILE="/tmp/.my.cnf"
+echo "[client]
+user=$DB_USER
+password=$DB_PASSWORD
+host=$DB_HOST" > "$CREDENTIALS_FILE"
 
-# Change to home directory
-cd
+sudo mysqldump --defaults-file="$CREDENTIALS_FILE" --single-transaction --default-character-set=utf8mb4 "$DB_NAME" > "$BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX/nextcloud-sqlbkp.bak"
+rm -f "$CREDENTIALS_FILE"
 
-# Create zip of backup directory
-sudo zip -rq $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX $BACKUP_DIR_DATE_SUFFIX
+# Create tarball of backup directory
+sudo tar -czf "$BACKUP_DIR/nextcloud_backup_$BACKUP_DIR_DATE_SUFFIX.tar.gz" -C "$BACKUP_DIR" "$BACKUP_DIR_DATE_SUFFIX"
 
 # Cleanup backup directory
-sudo rm -rf $BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX
+if [ $? -eq 0 ]; then
+    sudo rm -rf "$BACKUP_DIR/$BACKUP_DIR_DATE_SUFFIX"
+else
+    echo "Compression failed, not cleaning up intermediate files!" >&2
+    exit 1
+fi
 
 # Disable maintenance mode
-sudo -u www-data php --define apc.enable_cli=1 /var/www/nextcloud/occ maintenance:mode --off
+sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
+
