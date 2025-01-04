7c67b7cbbec24d707f4ce790851ca15fa449a502
Sourabh Pisal <pisal.sourabh@gmail.com>
2025-01-04 22:40:25 +0000

    fix backup to s3

--- backup-to-s3.sh
+++ backup-to-s3.sh
@@ -17,70 +17,11 @@ fi
 CONFIG_FILE="$NEXTCLOUD_DIR/config/config.php"
 DATA_DIR=$(grep "'datadirectory'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
 
-DB_NAME=$(grep "'dbname'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
-DB_HOST=$(grep "'dbhost'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
-DB_USER=$(grep "'dbuser'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
-DB_PASSWORD=$(grep "'dbpassword'" "$CONFIG_FILE" | awk -F "=> " '{print $2}' | tr -d "', ")
-
-# Enable maintenance mode
-echo "Enabling maintenance mode"
-sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --on
+# Sync directory to S3
+echo "Syncing directory to S3"
+aws s3 sync "$DATA_DIR/sourabh/files/" "s3://$S3_BUCKET/"
 if [ $? -ne 0 ]; then
-    echo "Failed to enable maintenance mode!" >&2
-    exit 1
-fi
-
-# Truncate log file
-echo "Truncating log file"
-sudo -u www-data truncate "$NEXTCLOUD_DIR/data/nextcloud.log" --size 0
-if [ $? -ne 0 ]; then
-    echo "Failed to truncate log file!" >&2
-    sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
-    exit 1
-fi
-
-# Take database snapshot
-CREDENTIALS_FILE="/tmp/.my.cnf"
-NEXTCLOUD_SQL_BKP="/tmp/nextcloud-sqlbkp.bak"
-
-echo "[client]
-user=$DB_USER
-password=$DB_PASSWORD
-host=$DB_HOST" > "$CREDENTIALS_FILE"
-
-echo "Taking database backup"
-sudo mysqldump --defaults-file="$CREDENTIALS_FILE" --single-transaction --default-character-set=utf8mb4 "$DB_NAME" > "$NEXTCLOUD_SQL_BKP"
-rm -f "$CREDENTIALS_FILE"
-if [ $? -ne 0 ]; then
-    echo "Database backup failed!" >&2
-    sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
-    exit 1
-fi
-
-# Sync data directory to S3
-echo "Syncing data directory to S3"
-aws s3 sync "$DATA_DIR/" "s3://$S3_BUCKET/data/" --delete
-if [ $? -ne 0 ]; then
-    echo "Data directory sync to S3 failed!" >&2
-    sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
-    exit 1
-fi
-
-# Sync config directory to S3
-echo "Syncing config directory to S3"
-aws s3 sync "$NEXTCLOUD_DIR/config/" "s3://$S3_BUCKET/config/" --delete
-if [ $? -ne 0 ]; then
-    echo "Config directory sync to S3 failed!" >&2
-    sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
-    exit 1
-fi
-
-# Sync database snapshot to S3
-echo "Syncing database backup to S3"
-aws s3 cp "$NEXTCLOUD_SQL_BKP" "s3://$S3_BUCKET/nextcloud-sqlbkp.bak"
-rm -f "$NEXTCLOUD_SQL_BKP"
-if [ $? -ne 0 ]; then
-    echo "Database backup sync to S3 failed!" >&2
+    echo "Directory sync to S3 failed!" >&2
     sudo -u www-data php --define apc.enable_cli=1 "$NEXTCLOUD_DIR/occ" maintenance:mode --off
     exit 1
 fi
